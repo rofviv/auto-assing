49555cc807cea80b18030aecdbd026b85cfd0783
rofviv <royvillarroel94@gmail.com>
2020-06-26 17:04:33 -0400

    se aumento la cantidad de merchant

--- src/pages/CallCenter.vue
+++ src/pages/CallCenter.vue
@@ -396,7 +396,7 @@ export default {
     async getMerchants() {
       this.listMerchant = [];
       const time = Date.now();
-      const cantMaxMerchant = 900;
+      const cantMaxMerchant = 1200;
       const URI = "https://prod-fresh-api.jugnoo.in:4040/panel/fetch_restaurants?token=" + this.access_token + "&secret=P7JlZXiRiIvSssQSSzqs&city=" + this.citySelect + "&sEcho=1&iColumns=11&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength="+ cantMaxMerchant +"&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=true&mDataProp_9=&sSearch_9=&bRegex_9=false&bSearchable_9=true&bSortable_9=true&mDataProp_10=10&sSearch_10=&bRegex_10=false&bSearchable_10=true&bSortable_10=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=" + time;
       try {
         const res = await this.$axios.get(URI);
